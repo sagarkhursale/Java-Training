6f9c7566b9e7de01b91ef53257c369a5ef182846
sagar khursale <khursule.sagar@gmail.com>
2018-12-14 14:16:20 +0530

    ss

---
+++ app/src/main/java/com/a247software/javatraining/MainActivity.java
@@ -0,0 +1,14 @@
+package com.a247software.javatraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
